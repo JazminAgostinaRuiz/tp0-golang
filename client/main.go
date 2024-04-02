76be15dd2225d3fc55658f3681f700971a14437a
JazminAgostinaRuiz <jaruiz@frba.utn.edu.ar>
2024-04-02 22:41:12 +0000

    Update main for server and client

--- client/main.go
+++ client/main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
-
+	log.Println("Soy un Log!")
 	// loggear "Hola soy un log" usando la biblioteca log
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
 	// validar que la config este cargada correctamente
 
 	// loggeamos el valor de la config
-
+	log.Println(globals.ClientConfig.Mensaje)
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// enviar un mensaje al servidor con el valor de la config
-
+	utils.EnviarMensaje(globals.ClientConfig.Ip, globals.ClientConfig.Puerto, globals.ClientConfig.Mensaje)
 	// leer de la consola el mensaje
-	// utils.LeerConsola()
-
+	mensajes := utils.LeerConsola()
+	log.Println(mensajes)
+	utils.GenerarYEnviarPaquete(mensajes, globals.ClientConfig.Ip, globals.ClientConfig.Puerto)
 	// generamos un paquete y lo enviamos al servidor
-	// utils.GenerarYEnviarPaquete()
 }
--- server/main.go
+++ server/main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func main() {
+	print("Iniciando")
 	mux := http.NewServeMux()
-
+	print("Escuchando .... ")
 	mux.HandleFunc("/paquetes", utils.RecibirPaquetes)
+	print("Se recibió un paquete")
 	mux.HandleFunc("/mensaje", utils.RecibirMensaje)
-
-	panic("no implementado!")
+	print("Se recibió un mensaje")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
+		print("Hubo error en el servidor")
 		panic(err)
 	}
 }
